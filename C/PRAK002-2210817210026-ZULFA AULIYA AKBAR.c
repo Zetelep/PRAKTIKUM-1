aa56b893048ebb24745bef1e888c2930385a2474
ZULFA AULIYA AKBAR <114343886+Zetelep@users.noreply.github.com>
2022-10-07 23:09:37 +0800

    Add files via upload

---
+++ C/PRAK002-2210817210026-ZULFA AULIYA AKBAR.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Selamat Pagi,  Zulfa Auliya Akbar \n");
+    printf("Selamat Siang, Zulfa Auliya Akbar \n");
+    printf("Selamat Malam, Zulfa Auliya Akbar \n");
+
+    return 0;
+}
+
---
+++ C/PRAK004-2210817210026-ZULFA AULIYA AKBAR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf(" ################################\n ");
+    printf("#                              # \n ");
+    printf("#       Zulfa Auliya Akbar     # \n ");
+    printf("#         2210817210026        # \n ");
+    printf("#                              # \n ");
+    printf("################################ \n ");
+
+    return 0;
+}
